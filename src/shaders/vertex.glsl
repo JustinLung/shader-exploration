7e22efc46b72178acbdfb13ba75f4b6f8d3cbc3b
JustinLung <justinlung77@gmail.com>
2023-05-01 16:53:11 +0200

    updated vertex

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -9,7 +9,7 @@ void main()
     vec4 modelPosition = modelMatrix * vec4(position, 1.5);
     modelPosition.y += sin(modelPosition.x * uFrequency.x - aRandom) * 0.02;
     modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.05;
-    modelPosition.x += sin(modelPosition.z * uFrequency.y - aRandom) * 0.01;
+    modelPosition.x += sin(modelPosition.z * uFrequency.y - aRandom) * 0.02;
     modelPosition.z += sin(aRandom * 0.2) * 1.0;
 
     vec4 viewPosition = viewMatrix * modelPosition;
